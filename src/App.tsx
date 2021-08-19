6fe08b265e68eaf9c8c27699d64775c713ecef5e
Alexandra Moore <alexandra.moore@kurtgeiger.com>
2021-08-19 16:34:29 +0100

    add Auth0 containing wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Auth0Provider } from "@auth0/auth0-react";
 import './App.css';
 import Routes from './Routes';
 import Header from './components/Header';
@@ -6,8 +7,14 @@ import Header from './components/Header';
 function App() {
     return (
         <div className="App">
-            <Header />
-            <Routes />
+            <Auth0Provider
+                domain={process.env.AUTH_DOMAIN || ''}
+                clientId={process.env.AUTH_CLIENTID || ''}
+                redirectUri={window.location.origin}
+            >
+                <Header />
+                <Routes />
+            </Auth0Provider>
         </div>
     );
 }
