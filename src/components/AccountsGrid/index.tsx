793acf4c7d4a4d78ce38919003da8e3b2890d098
Alexandra Moore <alexmoore94@hotmail.co.uk>
2021-08-29 17:46:25 +0100

    Create logic to sort accounts into columns

--- src/components/AccountsGrid/index.tsx
+++ src/components/AccountsGrid/index.tsx
@@ -5,10 +5,19 @@ import { Link } from 'react-router-dom';
 import AccountCard from '../../components/AccountCard';
 import { Account } from '../../types';
 
-import { StyledAccountsGridContainer } from './styles';
+import { StyledAccountsGridContainer, StyledColumn } from './styles';
 
 const AccountsGrid = () => {
-    const [accounts, setAccounts] = useState([]);
+    interface populatedColTypes {
+        date?: string;
+        accounts?: Account[];
+    };
+
+    type populatedColArray = populatedColTypes[];
+
+    const [accounts, setAccounts] = useState<Account[]>([]);
+    const [columns, setColumns] = useState<string[]>([]);
+    const [populatedColumns, setPopulatedColumns] = useState([] as populatedColArray);
     const { user } = useAuth0();
       
     useEffect(() => {
@@ -20,19 +29,58 @@ const AccountsGrid = () => {
         fetchAccounts();
     }, [user]);
 
+    // Seperate months out of accounts.
+    useEffect(() => {
+        let months: string[] = [];
+
+        accounts.forEach((account: Account) => {
+            let accountDate = account.month + ' ' + account.year;
+
+            if (!months.includes(accountDate)) {
+                months.push(accountDate);
+            };
+        });
+
+        setColumns(months);
+    }, [accounts]);
+
+    // Populate columns with accounts.
+    useEffect(() => {
+        let newArray = columns.map((column) => ({ date: column, accounts: [] as Account[]}));
+        
+        accounts.forEach((account: Account) => {
+            let accountDate = account.month + ' ' + account.year;
+
+            newArray.forEach((column) => {
+                if (accountDate === column.date) {
+                    column.accounts.push(account);
+                }
+            })
+        })
+        setPopulatedColumns(newArray as populatedColArray);
+    }, [columns, accounts]);
+
+    // Calculate totals within columns.
+
+
     return (
         <StyledAccountsGridContainer>
-            <Link to={'/create'}>+ Add a new account</Link>
-            {accounts && accounts.map((account: Account) => (
-            <div key={account._id}>
-                <AccountCard 
-                    _id={account._id}
-                    title={account.title}
-                    amount={account.amount}
-                    month={account.month}
-                    year={account.year}
-                />
-            </div>
+            { populatedColumns.length && populatedColumns.map((column: populatedColTypes, i: number) => (
+                <StyledColumn key={i}>
+                    <h3>{column.date}</h3>
+                        {column.accounts?.length && column.accounts.map((account: Account) => (
+                            <div key={account._id}>
+                                <AccountCard 
+                                    _id={account._id}
+                                    title={account.title}
+                                    amount={account.amount}
+                                    month={account.month}
+                                    year={account.year}
+                                />
+                            </div>
+                        ))}
+                    <Link to={'/create'}>+ Add a new account</Link>
+                </StyledColumn>
             ))}
         </StyledAccountsGridContainer>
     );
--- src/components/AccountsGrid/styles.ts
+++ src/components/AccountsGrid/styles.ts
@@ -2,5 +2,9 @@ import styled from 'styled-components';
 
 export const StyledAccountsGridContainer = styled.div`
     display: flex;
+`;
+
+export const StyledColumn = styled.div`
+    display: flex;
     flex-direction: column;
 `;
