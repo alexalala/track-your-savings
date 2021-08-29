1209ca7ecbe203da45746ab0f437b416caf5d2b6
Alexandra Moore <alexmoore94@hotmail.co.uk>
2021-08-30 00:27:25 +0100

    make landing page presentable

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 
+import stacks from '../../assets/notes.svg';
+import AuthButton from '../../components/AuthButton';
+import { StyledBackgroundContainer, StyledDescriptionContainer, StyledTextContainer, StyledStacks } from './styles';
+
 const Landing = () => (
     <div>
-        <h1>Hi I'm the Landing Page</h1>
+        <StyledBackgroundContainer />
+        <StyledDescriptionContainer>
+            <StyledTextContainer>
+                <h1>Track your savings on a monthly basis</h1>
+                <p>Ensure you keep on top of your monthly savings by using this handy tracking tool.</p>
+                <AuthButton signup={true} />
+            </StyledTextContainer>
+            <StyledStacks src={stacks} alt='' />
+        </StyledDescriptionContainer>
     </div>
 );
 
---
+++ src/pages/Landing/styles.ts
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const StyledBackgroundContainer = styled.div`
+    width: 100vw;
+    height: calc(100vh - 84px);
+    background: rgb(197,219,245);
+    background: linear-gradient(0deg, rgba(197,219,245,1) 0%, rgba(255,255,255,1) 100%);
+    position: relative;
+`;
+
+export const StyledDescriptionContainer = styled.div`
+    position: absolute;
+    top: 20vh;
+    display: flex;
+    align-items: center;
+    width: 100%;
+`;
+
+export const StyledStacks = styled.img`
+    width: 50%;
+`;
+
+export const StyledTextContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    padding: 0 3rem;
+
+    h1 {
+        text-transform: uppercase;
+        font-size: 3.5rem;
+        text-align: left;
+    }
+
+    p {
+        text-align: left;
+    }
+
+    button {
+        width: fit-content;
+    }
+`;
